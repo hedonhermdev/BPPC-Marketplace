a0bdb841bbfcfd798f5eb648e98f6db99f648e40
sarthak-choudhary <f20190112@pilani.bits-pilani.ac.in>
2020-03-29 02:05:25 +0530

    more views added

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('api/add_product/',content.add_product,name='add-product'),
     path('api/get_product_detail/<int:id>',content.product_detail,name='product-detail'),
     path('api/get_profile/<int:id>',content.get_profile,name='get-profile'),
+    path('api/get_product_detail/sell_product/<int:id>',content.sell_product,name='sell-product'),
+    path('api/get_user_products/<int:id>',content.user_products,name='user-products'),
 
     path('auth/login/',auth.login,name='auth-login'),
     path('auth/register/',auth.register,name='auth-register'),
--- main/views/content.py
+++ main/views/content.py
@@ -20,7 +20,7 @@ def add_product(request):
     data = request.data
     product = Product()
     if data['price'] is None:
-        return Response('Product should have a Price.',status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error':'Product should have a Price.'},status=status.HTTP_400_BAD_REQUEST)
     else :
         validated_price = data['price']
 
@@ -39,7 +39,7 @@ def product_detail(request,id):
     try:
         required_product = Product.objects.get(pk=id)
     except Product.DoesNotExist:
-        return Response('Product not found',status=status.HTTP_404_NOT_FOUND)
+        return Response({'error':'Product not found'},status=status.HTTP_404_NOT_FOUND)
 
     return Response(required_product.to_dict(),status=status.HTTP_200_OK)
 
@@ -49,8 +49,34 @@ def get_profile(request,id):
     try:
         required_user = User.objects.get(pk=id)
     except User.DoesNotExist:
-        return Response('No Such User Exist',status=status.HTTP_404_NOT_FOUND)
+        return Response({'error':'No Such User Exist'},status=status.HTTP_404_NOT_FOUND)
 
     required_profile = required_user.profile
 
     return Response(required_profile.to_dict(),status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def sell_product(request,id):
+    try:
+        required_product = Product.objects.get(pk=id)
+    except Product.DoesNotExist:
+        return Response({'error':'No Such Product Exits.'},status=status.HTTP_404_NOT_FOUND)
+    
+    if request.user.id != required_product.seller.id:
+        return Response({'error':'Only seller can sell a product'},status=status.HTTP_400_BAD_REQUEST)
+    
+    required_product.sold = True
+    return Response(required_product.to_dict(),status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_products(request,id):
+    try:
+        required_user = User.objects.get(pk=id)
+    except User.DoesNotExist:
+        return Response({'error':'No Such User Exist'},status=status.HTTP_404_NOT_FOUND)
+
+    my_products = [p.to_dict() for p in Product.objects.filter(seller_id=id)]
+    return Response(my_products,status=status.HTTP_200_OK)
+
