980ecc341d89f283c9365aabaee1cb92f72e5c5b
Anshal Shukla <shukla.anshal85@gmail.com>
2020-03-28 23:25:01 +0530

    Models improvised.

--- main/models.py
+++ main/models.py
@@ -2,22 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 
 HOSTEL_CHOICES = (
-    ("SR","SR Bhavan"),
-    ("RP","Rana Pratap Bhavan"),
-    ("GN","Gandhi Bhavan"),
-    ("KR","Krishna Bhavan"),
-    ("MR","Meera Bhavan"),
-
+    ("SR", "SR Bhavan"),
+    ("RP", "Rana Pratap Bhavan"),
+    ("GN", "Gandhi Bhavan"),
+    ("KR", "Krishna Bhavan"),
+    ("MR", "Meera Bhavan"),
 )
+
+CATEGORY_CHOICES = (
+    ("STAT", "Stationay"),
+    ("MOVI", "Movie Ticket"),
+    ("GRUB", "Grub Ticket"),
+    ("ELEC", "Electronics"),
+    ("CLOT", "Clothing"),
+    ("OTHR", "Other Utility"),
+)
+
+
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    hostel = models.CharField(choices=HOSTEL_CHOICES,max_length=2)
+    hostel = models.CharField(choices=HOSTEL_CHOICES, max_length=2)
     room_no = models.PositiveIntegerField()
     contact_no = models.PositiveIntegerField()
-    rating = models.DecimalField(max_digits=2,decimal_places=1,null=True,blank=True)
-    no_of_ratings = models.IntegerField(default=0)
+    rating = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
+    no_of_ratings = models.PositiveIntegerField(default=0)
     email = models.EmailField()
 
     def save(self):
@@ -31,63 +41,78 @@ class Profile(models.Model):
         no_of_ratings = 0
         for r in raters:
             rate_points = r.rating + rate_points
-            no_of_ratings = no_of_ratings +1
-        
+            no_of_ratings = no_of_ratings + 1
+
         self.no_of_ratings = no_of_ratings
-        rating = rate_points/no_of_ratings
-        self.rating = round(rating,1)
+        rating = rate_points / no_of_ratings
+        self.rating = round(rating, 1)
 
         super.save()
 
     def to_dict(self):
-        return{
-            'pk': self.pk,
-            'user': self.user.pk,
-            'name': self.name,
-            'hostel': self.hostel,
-            'room_no': self.room_no,
-            'contact_no': self.contact_no,
-            'rating': self.rating,
-            'no_of_rating': self.no_of_ratings,
-            'email': self.email,
-        
+        return {
+            "pk": self.pk,
+            "user": self.user.pk,
+            "name": self.name,
+            "hostel": self.hostel,
+            "room_no": self.room_no,
+            "contact_no": self.contact_no,
+            "rating": self.rating,
+            "no_of_rating": self.no_of_ratings,
+            "email": self.email,
         }
 
+    def __str__(self):
+        return self.user
+
+
 class RateUsers(models.Model):
-    rating_for = models.ForeignKey(User,related_name='ratings_recieved',on_delete=models.PROTECT)
-    rated_by = models.ForeignKey(User,related_name='rating_given',on_delete=models.PROTECT)
+    rating_for = models.ForeignKey(
+        User, related_name="ratings_recieved", on_delete=models.PROTECT
+    )
+    rated_by = models.ForeignKey(
+        User, related_name="rating_given", on_delete=models.PROTECT
+    )
     rating = models.IntegerField()
+
+    def __str__(self):
+        return self.rating_for.username
+
 
 class ProductManager(models.Manager):
     def tickets():
-        return super(ProductManager,self).get_query_set().filter(is_ticket=True)
+        return super(ProductManager, self).get_query_set().filter(is_ticket=True)
+
 
 class Product(models.Model):
-    seller = models.ForeignKey(User,related_name='my_items',on_delete=models.CASCADE)
+    seller = models.ForeignKey(User, related_name="my_items", on_delete=models.CASCADE)
     name = models.CharField(max_length=60)
-    price = models.IntegerField(blank=False,null=False)
+    price = models.IntegerField(blank=False, null=False)
     description = models.CharField(max_length=300)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=4, null=True)
     interested_buyers = models.ManyToManyField(User)
     sold = models.BooleanField(default=False)
     is_ticket = models.BooleanField(default=False)
-    
+
+    reported_by = models.ForeignKey(
+        User, related_name="products_reported", on_delete=models.PROTECT, null=True,
+    )
+
     retrieve = ProductManager()
     objects = models.Manager()
 
     def to_dict(self):
-        return{
-            'pk': self.pk,
-            'seller': self.seller.pk,
-            'price': self.price,
-            'description': self.description,
-            'interested_buyers': [i.profile.to_dict() for i in self.interested_buyers.all()],
-            'sold': self.sold,
-            'is_ticket': self.is_ticket,
+        return {
+            "pk": self.pk,
+            "seller": self.seller.pk,
+            "price": self.price,
+            "description": self.description,
+            "interested_buyers": [
+                i.profile.to_dict() for i in self.interested_buyers.all()
+            ],
+            "sold": self.sold,
+            "is_ticket": self.is_ticket,
         }
 
-
- 
-
-
-        
-
+    def __str__(self):
+        return self.name
