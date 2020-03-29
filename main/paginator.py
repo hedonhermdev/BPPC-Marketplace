ba206490f62b354872c56980923c772be6be7cd1
Anshal Shukla <shukla.anshal85@gmail.com>
2020-03-29 16:33:18 +0530

    Pagination

--- main/paginator.py
+++ main/paginator.py
@@ -3,6 +3,6 @@ from rest_framework.pagination import CursorPagination
 
 class CustomCursorPagination(CursorPagination):
     page_size = 2
-    cursor_query_param = "cursor"
+    cursor_query_param = "page"
     ordering = "-created"
     template = None
