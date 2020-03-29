c0d2766e03fb6391713821bcac101727716fe9d8
Anshal Shukla <shukla.anshal85@gmail.com>
2020-03-29 19:19:52 +0530

    Revert ".gitignore added."
    This reverts commit 20e86a60d1acadec59b1f704f21352030f923345, reversing
    changes made to ba206490f62b354872c56980923c772be6be7cd1.

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import content, auth
 
 urlpatterns = [
-<<<<<<< HEAD
     path("api/get_products/", content.get_products, name="get-products"),
     path("api/add_product/", content.add_product, name="add-product"),
     path(
@@ -19,12 +18,4 @@ urlpatterns = [
     path("auth/login/", auth.login, name="auth-login"),
     path("auth/register/", auth.register, name="auth-register"),
 ]
-=======
-    path('api/get_products/',content.get_products,name='get-products'),
-    path('api/add_product/',content.add_product,name='add-product'),
-    path('api/get_product_detail/<int:id>',content.product_detail,name='product-detail'),
-    path('api/get_profile/<int:id>',content.get_profile,name='get-profile'),
-    path('api/get_product_detail/sell_product/<int:id>',content.sell_product,name='sell-product'),
-    path('api/get_user_products/<int:id>',content.user_products,name='user-products'),
->>>>>>> 5235e4b6758e5ff17ec01751154a3fa911b4785b
 
