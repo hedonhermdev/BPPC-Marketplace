7ee36f73de3f1ce50e13723617f011b40967ab31
Anshal Shukla <shukla.anshal85@gmail.com>
2020-03-30 19:00:34 +0530

    pagination,caching,atomic transaction

--- main/views/content.py
+++ main/views/content.py
@@ -3,34 +3,44 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
-
+from rest_framework.pagination import PageNumberPagination
+from django.views.decorators.cache import cache_page
+from django.db import transaction
 
 from django.contrib.auth.models import User
 from main.models import Profile, Product
-from main.paginator import CustomCursorPagination
 
 
-@api_view(["GET",])
+@api_view(
+    ["GET",]
+)
 @permission_classes([IsAuthenticated])
+@transaction.atomic
+@cache_page(60 * 5)
 def get_products(request):
-    paginator = CustomCursorPagination()
+    paginator = PageNumberPagination()
     query_set = Product.objects.all()
     context = paginator.paginate_queryset(query_set, request)
     products = [p.to_dict() for p in context]
     return Response(products, status=status.HTTP_200_OK)
 
 
-@api_view(["POST",])
+@api_view(
+    ["POST",]
+)
 @permission_classes([IsAuthenticated])
+@transaction.atomic
 def add_product(request):
     data = request.data
     product = Product()
     if data["price"] is None:
-        return Response({'error':'Product should have a Price.'},status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"error": "Product should have a Price."},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
     else:
         validated_price = data["price"]
-    
+
     product.seller = request.user
     product.price = validated_price
     product.interested_buyers = data["interested_buyers"]
@@ -41,24 +51,32 @@ def add_product(request):
     return Response(product.to_dict(), status=status.HTTP_201_CREATED)
 
 
-@api_view(["GET",])
+@cache_page(60 * 10)
+@api_view(
+    ["GET",]
+)
 @permission_classes([IsAuthenticated])
 def product_detail(request, id):
     try:
         required_product = Product.objects.get(pk=id)
     except Product.DoesNotExist:
-        return Response({'error':'Product not found'},status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {"error": "Product not found"}, status=status.HTTP_404_NOT_FOUND
+        )
 
     return Response(required_product.to_dict(), status=status.HTTP_200_OK)
 
 
+@cache_page(60 * 10)
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def get_profile(request, id):
     try:
         required_user = User.objects.get(pk=id)
     except User.DoesNotExist:
-        return Response({'error':'No Such User Exist'},status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {"error": "No Such User Exist"}, status=status.HTTP_404_NOT_FOUND
+        )
 
     required_profile = required_user.profile
 
@@ -70,7 +88,9 @@ def get_profile(request, id):
 def rate_user(request):
     data = request.data
     if data["rating"] is None:
-        return Response({'error':"seller is not rated."}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"error": "seller is not rated."}, status=status.HTTP_400_BAD_REQUEST
+        )
 
     RateUsers(
         rating_for=data["seller"], rated_by=request.user, rating=data["rating"]
@@ -80,6 +100,7 @@ def rate_user(request):
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
+@cache_page(60 * 5)
 def search_product(request):
     data = request.data
     products = Product.objects.all()
@@ -92,6 +113,7 @@ def search_product(request):
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
+@cache_page(60 * 5)
 def interested_buyers(request, id):
     interested = request.data["interested_buyer"]
     product = Product.objects.get(pk=id)
@@ -101,48 +123,60 @@ def interested_buyers(request, id):
         product.interested_buyers.add(interested)
     return Response(product.to_dict(), status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
-def sell_product(request,id):
+def sell_product(request, id):
     try:
         required_product = Product.objects.get(pk=id)
     except Product.DoesNotExist:
-        return Response({'error':'No Such Product Exits.'},status=status.HTTP_404_NOT_FOUND)
-    
-    if request.user.id != required_product.seller.id:
-        return Response({'error':'Only seller can sell a product'},status=status.HTTP_400_BAD_REQUEST)
-    
-    required_product.sold = True
-    return Response(required_product.to_dict(),status=status.HTTP_200_OK)
+        return Response(
+            {"error": "No Such Product Exits."}, status=status.HTTP_404_NOT_FOUND
+        )
 
-@api_view(['GET'])
+    if request.user.id != required_product.seller.id:
+        return Response(
+            {"error": "Only seller can sell a product"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    required_product.sold = True
+    return Response(required_product.to_dict(), status=status.HTTP_200_OK)
+
+
+@cache_page(60 * 10)
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
-def user_products(request,id):
+def user_products(request, id):
     try:
         required_user = User.objects.get(pk=id)
     except User.DoesNotExist:
-        return Response({'error':'No Such User Exist'},status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {"error": "No Such User Exist"}, status=status.HTTP_404_NOT_FOUND
+        )
 
     my_products = [p.to_dict() for p in Product.objects.filter(seller_id=id)]
-    return Response(my_products,status=status.HTTP_200_OK)
+    return Response(my_products, status=status.HTTP_200_OK)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def my_profile(request):
     my_profile = request.user.profile
-    return Response(my_profile.to_dict(),status=status.HTTP_200_OK)
+    return Response(my_profile.to_dict(), status=status.HTTP_200_OK)
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def update_profile(request):
     user = request.user
     data = request.data
-    user.profile.name = data['name']
-    user.profile.hostel = data['hostel']
-    user.profile.room_no = data['room_no']
-    user.profile.contact_no = data['contact_no']
-    user.profile.email = data['email']
+    user.profile.name = data["name"]
+    user.profile.hostel = data["hostel"]
+    user.profile.room_no = data["room_no"]
+    user.profile.contact_no = data["contact_no"]
+    user.profile.email = data["email"]
 
     user.save()
 
-    return Response(user.profile.to_dict(),status=status.HTTP_200_OK)
+    return Response(user.profile.to_dict(), status=status.HTTP_200_OK)
--- marketplace/settings.py
+++ marketplace/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zg(od63#s(ix=)12#f=q6asvox!o0#vm5_fu^ahsl!md&=kjxa'
+SECRET_KEY = "zg(od63#s(ix=)12#f=q6asvox!o0#vm5_fu^ahsl!md&=kjxa"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,83 +31,84 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'main',
-    'rest_framework',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "main",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'marketplace.urls'
+ROOT_URLCONF = "marketplace.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'marketplace.wsgi.application'
+WSGI_APPLICATION = "marketplace.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
-
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient",},
+        "KEY_PREFIX": "example",
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -119,16 +120,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 3,
 }
 
 JWT_AUTH = {
-    'JWT_VERIFY_EXPIRATION': False,
+    "JWT_VERIFY_EXPIRATION": False,
 }
