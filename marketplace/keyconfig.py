1c394f8b0e80e1d69f74bfc097720461c810e5ca
Tirth Jain <jaintirth24@gmail.com>
2020-04-15 01:28:33 +0530

    host

--- marketplace/keyconfig.py
+++ marketplace/keyconfig.py
@@ -1,6 +1,7 @@
 class Secrets:
     HOST_DOMAIN = "market.bits-dvm.org"
     HOST_IP = "157.245.3.147"
+    HOST_RESOLVER = "www.iparv.com.br"
     SECRET_KEY = "zg(od63#s(ix=)12#f=q6asvox!o0#vm5_fu^ahsl!md&=kjxa"
 
 
--- marketplace/settings.py
+++ marketplace/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = Secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", Secrets.HOST_DOMAIN, Secrets.HOST_IP]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", Secrets.HOST_DOMAIN, Secrets.HOST_IP, Secrets.HOST_RESOLVER]
 
 
 # Application definition
