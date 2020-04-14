2e2dba108a6f0b5adf348e2711fd30db1bc77a12
Tirth Jain <jaintirth24@gmail.com>
2020-04-15 02:22:44 +0530

    host

--- marketplace/keyconfig.py
+++ marketplace/keyconfig.py
@@ -1,5 +1,5 @@
 class Secrets:
-    HOST_DOMAIN = "marketplace.bits-dvm.org"
+    HOST_DOMAIN = "market.bits-dvm.org"
     HOST_IP = "157.245.3.147"
     HOST_RESOLVER = "www.iparv.com.br"
     SECRET_KEY = "zg(od63#s(ix=)12#f=q6asvox!o0#vm5_fu^ahsl!md&=kjxa"
