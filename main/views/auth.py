bcb184cd2f1ef20ae989e0481fc3b4a6de47cabb
Tirth Jain <jaintirth24@gmail.com>
2020-04-16 19:15:39 +0530

    Remove login view

--- main/views/auth.py
+++ main/views/auth.py
@@ -12,46 +12,6 @@ import logging
 log = logging.getLogger("main")
 
 
-@api_view(["POST"])
-def login(request):
-    try:
-        id_token = request.data["id_token"]
-    except KeyError:
-        log.error(f"{request.path}: no id_token provided in request body. ")
-        return Response(
-            {"error": "No id_token provided"}, status=status.HTTP_403_FORBIDDEN
-        )
-
-    id_info = googleIdToken.verify_oauth2_token(id_token, google_requests.Request())
-
-    if id_info["iss"] not in ["accounts.google.com", "https://accounts.google.com"]:
-        return Response(
-            {"error": "Not a valid Google account"}, status=status.HTTP_403_FORBIDDEN
-        )
-
-    email = id_info["email"]
-
-    domain = getattr(id_info, "hd", email.split("@")[-1])
-
-    if domain != "pilani.bits-pilani.ac.in":
-        log.error(f"{request.path}: {email}is not a valid BITS Mail account.")
-        return Response(
-            {"error": "Not a valid BITS Mail account"}, status=status.HTTP_403_FORBIDDEN
-        )
-
-    try:
-        user = User.objects.get(email=email)
-    except User.DoesNotExist:
-        log.error(f"{request.path}: {email} is not registered.")
-        return Response(
-            {"error": "Account not found. You must register first."},
-            status=status.HTTP_403_FORBIDDEN,
-        )
-
-    token = get_jwt_with_user(user)
-    log.info(f"{request.path}: user {user.username} logged in.")
-    return Response({"token": token}, status=status.HTTP_200_OK)
-
 @api_view(['POST'])
 def authenticate(request):
     try:
