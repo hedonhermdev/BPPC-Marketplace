67c01755a295ac20edffd0bf3b4fc4ebb4bc2508
Tirth Jain <jaintirth24@gmail.com>
2020-04-18 03:02:03 +0530

    Remove keyconfig

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from marketplace.keyconfig import Secrets, Elasticsearch, PostgresDB
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
