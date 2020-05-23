97f0fce7cb22f3a926f0235c8bc4b10a307175c5
darmis007 <f20190663@pilani.bits-pilani.ac.in>
2020-05-23 21:27:12 +0530

    product_tests added

--- backend/main/tests/test_models.py
+++ backend/main/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from main.models import User, Profile, ProfileRating
+from main.models import User, Profile, ProfileRating, Category, Product
 from main.auth_helpers import create_user_from_email
 
 import random
@@ -55,3 +55,95 @@ class TestProfileRating(TestCase):
         rating.save()
         # Test that user's profile rating is updated.
         self.assertEqual(profile1.rating, RATING)
+
+class TestProduct(TestCase):
+    def create_test_user(self,email):
+        return create_user_from_email(email)
+
+    def create_test_category(self,name):
+        category=Category(name=name)
+        return category
+    
+    def create_test_product(self,name='bbc'):
+        product_name=self.name
+        profile1 = self.create_test_user("userone@gmail.com").profile
+        basePrice = 100
+        description = "Some unknown english words"
+        category = create_test_category(name='xyzCategory')
+        product = Product(name=product_name,seller=profile1,
+                        base_price=basePrice,description=description,
+                        category=category)
+        product.save()
+        return product
+    
+    def product_is_complete(self):
+        product = Product()
+        self.assertEqual(product.is_complete, False)
+        product.name= "XYZ"
+        product.base_price = 200
+        product.description = "Description"
+        product.category = self.create_test_category(name='xxyz')
+        product.seller = self.create_test_user("userone@gmail.com").profile
+        product.save()
+        self.assertEqual(product.is_complete, True)
+
+    def product_is_visible(self):
+        product = self.create_test_product()
+        self.assertEqual(product.is_visible, True)
+        product.is_visible = False
+        product.save()
+        self.assertEqual(product.is_visible, False)
+
+    def product_is_ticket(self):
+        product = self.create_test_product()
+        self.assertEqual(product.is_ticket, False)
+        product.is_ticket = True
+        product.save()
+        self.assertEqual(product.is_ticket, True)
+
+    def product_is_sold(self):
+        product = self.create_test_product()
+        self.assertEqual(product.is_visible, False)
+        product.is_visible = True
+        product.save()
+        self.assertEqual(product.is_visible, True)
+
+    def add_product_no_description(self):
+        product=Product()
+        is_sellable=True
+        product.name= "XYZ"
+        product.base_price = 20
+        product.description = ""
+        product.category = self.create_test_category(name='xxyz')
+        product.seller = self.create_test_user("userone@gmail.com").profile
+        try:
+            product.save()
+        except:
+            is_sellable=False
+        self.assertEqual(is_sellable,False)
+
+
+
+
+    
+
+
+
+    
+    
+
+        
+        
+
+    
+
+
+
+
+
+
+
+
+
+
+
